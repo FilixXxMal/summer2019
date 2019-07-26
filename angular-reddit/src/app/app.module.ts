bb94f93e3691c90800785cb89afa903ad0d18ac0
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-07-26 19:27:55 +0000

    commit 7

--- angular-reddit/src/app/app.module.ts
+++ angular-reddit/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ArticleComponent } from './article/article.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+        BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
