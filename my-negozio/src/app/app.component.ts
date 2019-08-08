bdf3b4de93b661fc31df72da1a6e7f1af865c5e2
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-08-08 14:56:26 +0000

    commit 13

--- my-negozio/src/app/app.component.ts
+++ my-negozio/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-negozio-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-negozio';
+  product: Product;
+    constructor() {
+    this.product = new Product(
+      'NICEHAT',
+      'A Nice Black Hat',
+      '/resources/images/products/black-hat.jpg',
+      ['Men', 'Accessories', 'Hats'],
+      29.99);
+    }
+
 }
