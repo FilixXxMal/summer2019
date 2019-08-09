2a6786af86dcb371f8b6e3c617a8b026485224dd
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-08-09 09:02:26 +0000

    commit 17

--- my-negozio/src/app/app.component.ts
+++ my-negozio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { Product } from './product.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   products: Product[];
   conistructor() {
     this.products = [
@@ -34,4 +34,6 @@ export class AppComponent {
     console.log('Product clicked: ', product);
   }
 
+  ngOnInit() {
+  }
 }
